dbcb36cae3adae0640d1a98969decfb9dea10349
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-27 13:59:30 -0300

    Criação e conclusão do Exercício 6.

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 6.py
@@ -0,0 +1,7 @@
+# 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input('Informe o raio do círculo: '))
+
+resultado = 3.14 * raio ** 2
+
+print(f'O círculo de raio {raio} tem uma área de {resultado}.')
