101daa3767c57227d63f45d5ac1aeca5788b59e9
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-27 13:58:31 -0300

    Criação e conclusão do Exercício 3.

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 3.py
@@ -0,0 +1,8 @@
+# 3 - Faça um programa que peça dois números e imprima a soma.
+
+num_1 = float(input('Digite um número: '))
+num_2 = float(input('Digite outro número: '))
+
+soma = num_1 + num_2
+
+print(f'A soma entre os dois números é: {soma:.2f},')
