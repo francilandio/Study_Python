1c61645d1f0dc07212fae403c8fd3031f5545a64
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-27 13:58:59 -0300

    Criação e conclusão do Exercício 5.

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 5.py
@@ -0,0 +1,8 @@
+# Faça um programa que converta metros para centímetros.
+
+metros = float(input('Quantos metros deseja converter em centímetros: '))
+
+centimetro = metros * 100
+
+print(f'{metros} metro(s) é equivalente a {centimetro:.2f} centímetro(s). ')
+
