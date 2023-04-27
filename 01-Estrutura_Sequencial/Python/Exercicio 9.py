7eb5411ef4bd6a224c87e44665bf8adc1fd7365c
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-27 16:38:34 -0300

    Conclusão do Exercício 9

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 9.py
@@ -0,0 +1,12 @@
+# 9 - Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+# C = 5 * ((F-32) / 9).
+
+print('########## CONVERSOR FAHRENHEIT PARA CELSIUS ##########')
+print()
+graus_f = float(input('Informe a temperatura: '))
+
+graus_c = 5 * ((graus_f-32) / 9)
+
+print(f'O valor de {graus_f} °F equivalem a {graus_c} °C.')
+print()
+print('########## FIM ##########')
