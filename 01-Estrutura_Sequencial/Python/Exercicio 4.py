2f317afe12bfcc94abf44e1409dc094568fb421e
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-27 13:58:44 -0300

    Criação e conclusão do Exercício 4.

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 4.py
@@ -0,0 +1,10 @@
+# 4 - Faça um programa que peça as 4 notas bimestrais e mostre a média.
+
+nota_1 = float(input('Informe a nota do 1º bimestre: '))
+nota_2 = float(input('Informe a nota do 1º bimestre: '))
+nota_3 = float(input('Informe a nota do 1º bimestre: '))
+nota_4 = float(input('Informe a nota do 1º bimestre: '))
+
+media = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print(f'Sua média foi: {media:.2f}')
