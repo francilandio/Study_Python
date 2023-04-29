b978972b2b66b270b3248d90a26d3891b358cade
Francilandio Oliveira <francilandioo@gmail.com>
2023-04-28 21:58:09 -0300

    Criação e conclusão do Exercício 18.

---
+++ 01-Estrutura_Sequencial/Python/Exercicio 18.py
@@ -0,0 +1,14 @@
+# Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
+
+print('########## TEMPO DE DOWNLOAD ##########')
+print()
+
+tamanho_arquivo = int(input('Qual o tamanho do arquivo? '))
+velocidade = int(input('Qual a velocidade da sua internet? '))
+
+tempo = (tamanho_arquivo / (velocidade / 8)) / 60
+
+print(f'Para realizar o download de um arquivo de {tamanho_arquivo} MB, a uma velocidade de {velocidade} Mbps levará {tempo:.0f} minutos.')
+
+print()
+print('########## TEMPO DE DOWNLOAD ##########')
